202dda9d120fdb25607c2135fdba249695fe47ae
ukzshd <96689369+ukzshd@users.noreply.github.com>
2025-04-03 21:14:41 +0800

    defaults o3-mini to high

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -241,7 +241,6 @@ export class ChatGPTApi implements LLMApi {
 
       // O1 使用 max_completion_tokens 控制token数 (https://platform.openai.com/docs/guides/reasoning#controlling-costs)
       if (isO1OrO3) {
-        requestPayload["max_completion_tokens"] = modelConfig.max_tokens;
         requestPayload["max_completion_tokens"] = 25000;
       }
       
